33b2f92e9bbb58a8d820d42a87445c3b200702b5
Viktor Petukhov <viktor.petukhov+gh@pm.me>
2025-06-04 22:00:23 +0100

    Allowed Firefox, as it doesn't cause problems anymore

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom/client";
 
 import { App } from "./app";
 
-if (navigator.userAgent.indexOf("Firefox") !== -1) {
-  const erorr = "Please use Chrome or Safari. Firefox doesn't support the necessary functionality.";
-  alert(erorr);
-  throw new Error(erorr);
-}
-
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement as HTMLElement);
 root.render(
